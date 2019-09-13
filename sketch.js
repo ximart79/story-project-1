e7b0f2be25a3f9f9cbd6eba0674ec5bfebfb2159
xander <ximart79@harrisonburg.k12.va.us>
2019-09-13 09:47:13 -0400

    finshed the story

--- sketch.js
+++ sketch.js
@@ -5,14 +5,14 @@ function setup() {
   // Change these to change the title and author of the story
   document.getElementById('title').innerHTML="Goldilocks & the Three Bears"; // title
 
-  document.getElementById('author').innerHTML="Jon Stapleton"; // author
+  document.getElementById('author').innerHTML="xander"; // author
 
 
   
   // All of your code goes under here
   
 
-  var story = 'Goldilocks has been lost in the woods for ' + many() + ' hours. Eventually she discovers a house, and smells something delicious. ' + delicious(); // Create the text
+  var story = 'Goldilocks went on a walk for ' + many() + ' hours and she was lost. eventually she found a cave that had a carpet that said home sweet home. she was ' + wondering()+' is she was going  to go in the cave. she dose enter the cave and sees that there arre some bowles on the tabe. she is very hungry and she dose '+eat()+' eat the porridge.the bears get home and she runs away but the bears start chasing her down. she find the bridge that has a river that leads to her houe she thinks of' +thinking()+' and gets home to her family . the end.' // Create the text
 
   printToPage(story); // Print the text to the webpage
 }
@@ -29,21 +29,31 @@ function many() {
   return hours
 }
 
-function delicious() {
+function wondering() {
   if(veryHungry == true) {
-    return "Goldilocks is very hungry, so she goes inside."
+    return "gose inside the cave. she enters the cave and sees that there are some bowls on the table. she is very hungry and " +eat();
   } else {
-    let inspection = inspects();
-    return inspection;
+  return " dose not go inside the story ends";
   }
 }
 
-function inspects() {
+function eat() {
   let insp = random(1, 6);
-  if(insp > 5) {
-    return "Goldilocks sees fresh bear tracks leaving the house!"
+  if(insp > 3) {
+    return " she dose not eat the porriged and falls asleep in the little bears bed. "
   } else {
-    return "Goldilocks inspects the house and everything seems alright. She goes inside."
+    return " she eat the porriged. the bear gets home and she runs away but the bears start chasing her down. she find the bridge that lead to her house. she   "
+    +thinking();
+  }
+}
+
+
+function thinking() {
+  let insp = random(1, 6);
+  if(insp > 3) {
+  return " dose not jump she gets turned into the bears maid and never returns homr"
+  } else { 
+    return" jumps and gets home with her family. THE END "
   }
 }
 
